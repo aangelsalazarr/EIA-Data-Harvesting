b677923b74da0cd4ef03b06a415a071ef08c7248
Angel Salazar <angelsalazar@uchicago.edu>
2021-12-13 16:06:33 -0600

    setting up necessary libraries for EIA Data Harvest

---
+++ EIA_Data_Harvesting.py
@@ -0,0 +1,7 @@
+# installing necessary libraries
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import date
+import matplotlib.ticker as ticker
