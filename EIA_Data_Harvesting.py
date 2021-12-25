0a90d57a16723f572f4cdc5beb1caf2cf0ac6354
Angel Salazar <aangelsalazarr@gmail.com>
2021-12-25 14:09:21 -0600

    working on grabbing data from the world bank

--- EIA_Data_Harvesting.py
+++ EIA_Data_Harvesting.py
@@ -29,7 +29,6 @@ endDate = '2021-01-01'
 
 # pull in data via EIA API
 for i in range(len(PADD_KEY)):
-
     url = 'http://api.eia.gov/series/?api_key=' + api_key + '&series_id=' + \
           PADD_KEY[i]
 
@@ -42,13 +41,65 @@ for i in range(len(PADD_KEY)):
         print('Error :(')
 
     df = pd.DataFrame(json_data.get('series')[0].get('data'),
-                      columns = ['Date', PADD_NAMES[i]])
+                      columns=['Date', PADD_NAMES[i]])
 
-    df.set_index('Date', drop = True, inplace = True)
+    df.set_index('Date', drop=True, inplace=True)
     final_data.append(df)
 
 # combine all the data into one df
-##crude = pd.concat(final_data, axis = 1)
+crude = pd.concat(final_data, axis=1)
 
 # create date as datetype datatype
-##crude['Year'] = crude.index
+crude['Year'] = crude.index.astype(str).str[:4]
+crude['Month'] = crude.index.astype(str).str[:4]
+crude['Day'] = 1
+crude['Date'] = pd.to_datetime(crude[['Year', 'Month', 'Day']])
+crude.set_index('Date', drop=True, inplace=True)
+crude.sort_index(inplace=True)
+crude = crude[startDate:endDate]
+crude = crude.iloc[:, :5]
+
+# Generating Colours and Style
+colors = {'PADD 1': '#045275',
+          'PADD 2': '#089099',
+          'PADD 3': '#7CCBA2',
+          'PADD 4': '#7C1D6F',
+          'PADD 5': '#DC3977'}
+
+plt.style.use('fivethirtyeight')
+
+# Creating the Visualization
+plot = crude.plot(figsize=(12, 8),
+                  color=list(colors.values()),
+                  linewidth=5,
+                  legend=False)
+
+plot.yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+plot.grid(color='#d4d4d4')
+plot.set_xlabel('Date')
+plot.set_ylabel('Crude Inventory Levels (KBD)')
+
+# Assigning Colour
+for padd in list(colors.keys()):
+    plot.text(x=crude.index[-1], y=crude[padd].max(), color=colors[padd], s=padd,
+              weight='bold')
+
+# Adding Labels
+    plot.text(x=crude.index[1],
+             y=int(crude.max().max()) + 1300,
+             s="Crude Consumption by PADD (thousand barrels per day)",
+             fontsize=23,
+            weight='bold',
+            alpha=.75)
+
+    plot.text(x=crude.index[1],
+             y=int(crude.max().max()) + 900,
+            s="Crude consumption by refineries and blenders is a proxy for crude demand in each "
+                "region",
+            fontsize=16,
+            alpha=.75)
+
+    plot.text(x=crude.index[1],
+             y=-1000,
+             s='Source: Energy Information Administration, www.eia.gov',
+             fontsize=10)
