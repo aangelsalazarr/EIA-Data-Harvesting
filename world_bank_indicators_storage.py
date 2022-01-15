28b9560b129e2eae0b86f22a0b4986613b4fb5ca
Angel Salazar <aangelsalazarr@gmail.com>
2022-01-14 23:46:09 -0600

    creating first portion of framework documenting all data keys

---
+++ world_bank_indicators_storage.py
@@ -0,0 +1,16 @@
+# purpose is to store world bank indicators that are relevant
+
+# energy and mining
+
+# electricity access
+electricity_access = "EG.ELC.ACCS.ZS"
+urban_electricity_access = "EG.ELC.ACCS.UR.ZS"
+rural_electricity_access = "EG.ELC.ACCS.RU.ZS"
+
+# energy consumption
+electric_power_consumption = "EG.USE.ELEC.KH.PC"
+fossil_fuel_energy_consumption = "EG.USE.COMM.FO.ZS"
+
+
+# energy production
+electricity_production_coal = "EG.ELC.COAL.ZS"
