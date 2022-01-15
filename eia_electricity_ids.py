8e1f235040da8fbbf60012447f12fbe184b8051c
Angel Salazar <aangelsalazarr@gmail.com>
2022-01-15 00:04:18 -0600

    creating the first portion of storing the eia series ids for further use down the road

---
+++ eia_electricity_ids.py
@@ -0,0 +1,48 @@
+# purpose is to store the series id of electricity relevant paths in the EIA open data website
+
+# -- net generation --
+# by fuel type, all fuels, electric power (total), monthly
+net_gen_all_fuels_electric_us = "ELEC.GEN.ALL-US-98.M"
+
+# by fuel type, all fuels, electric utility, monthly
+net_gen_all_fuels_electric_utility_us = "ELEC.GEN.ALL-US-1.M"
+
+#
+
+
+# -- total consumption --
+
+# -- total consumption (Btu) --
+
+# -- consumption for electricity generation --
+
+# -- consumption for electricity generation (Btu) --
+
+# -- consumption for useful thermal output --
+
+# -- consumption for useful thermal output (Btu) --
+
+# -- plant level data --
+
+# -- retail sales of electricity --
+
+# -- revenue from retail sales of electricity --
+
+# -- average retail price of electricity --
+
+# -- number of customer accounts --
+
+# -- fossil fuel stocks for electricity generation --
+
+# -- receipts of fossil fuels by electricity plants --
+
+# -- receipts of fossil fuels by electricity plants (Btu) --
+
+# -- average cost of fossil fuels for electricity generation --
+
+# -- average costs of fossil fuels for electricity generation (Btu) --
+
+# -- quality of fossil fuels in electricity generation : sulfur content --
+
+# -- quality of fossil fuels in electricity generation : ash content --
+
