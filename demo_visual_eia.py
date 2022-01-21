bfdbb4bd03453e8b5897fdf4d29d9396db1e1564
Angel Salazar <aangelsalazarr@gmail.com>
2022-01-20 20:21:20 -0600

    working on an EIA demo visualization - demo complete

--- demo_visual_eia.py
+++ demo_visual_eia.py
@@ -17,15 +17,30 @@ api = eia.API(api_key)
 # here is the code
 def retrieve_time_series(api, series_ID):
     """
-    Return the time series dataframe, based on API and unique Series ID
+    return the time series dataframe, based on API and a unique Series ID
     """
     # retrieve data by series ID
     series_search = api.data_by_series(series=series_ID)
 
     # create a pandas dataframe from the retrieved time series
-    df = pd.DataFrame(series_search)
+    df = pd.DataFrame(data=series_search)
+    df.reset_index(inplace=True)
+    df.columns = ['date', 'value']
     return df
 
 
+# calling EIA API with specified series ID
 plot1 = retrieve_time_series(api, net_gen_all_fuels_electric_us)
+print(plot1.head())  # plotting first few lines of our df
 print(plot1)
+
+# here we will attempt to visualize the plot
+plot1.plot(x='date', y='value', color='blue')
+
+# adding titles and axis names
+plt.title('US Electric Power, Net Generation')
+plt.ylabel('thousand megawatthours (MWh)')
+plt.xlabel('Date')
+
+# showing our plot
+plt.show()
