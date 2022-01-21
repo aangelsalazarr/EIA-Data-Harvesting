cbee77d601a1a4f12755b866876eab473ba9121e
Angel Salazar <aangelsalazarr@gmail.com>
2022-01-20 20:50:37 -0600

    working on worldbank data visualization demo - complete

--- world_bank.py
+++ world_bank.py
@@ -2,42 +2,47 @@ import pandas as pd
 from pandas_datareader import wb
 import matplotlib.pyplot as plt
 from functools import reduce
+import wbdata
 
 # want to grab electricity access data
 electricity_access = "EG.ELC.ACCS.ZS"
 renewable_electricity_excluding_hydro = "EG.ELC.RNWX.KH"
-renewable_energy_consumption = "EG.FEC.RNEW.ZS"
+renewable_energy_consumption = "EG.FEC.RNEW.ZS"  # % of total final energy consumption
 
-countries = ["USA"]
+countries = ["CHN"]
 
 # creating a dataframe on electricity access
 df1 = wb.download(indicator=electricity_access, country=countries, start=2010,
                   end=2018)
-# print(df1)
+#print("electricity access data")
+#print(df1, "\n")
 
 # creating a dataframe on renewable electricity excluding hydro
 df2 = wb.download(indicator=renewable_electricity_excluding_hydro, country=countries,
                   start=2010, end=2018)
-# print(df2)
+#print("Renewable Electricity")
+#print(df2, "\n")
 
 # creating a dataframe on renewable energy consumption (%)
-df3 = wb.download(indicator=renewable_energy_consumption, country=countries,
-                  start=2010, end=2018)
-# print(df3)
+df3 = pd.DataFrame(data=wb.download(indicator=renewable_energy_consumption, country=countries,
+                  start=2000, end=2018))
+df3.reset_index(inplace=True)
+df3.columns = ['country', 'year', 'value']
+df3['year'] = pd.to_datetime(df3['year'])
+df3 = df3.sort_values(by='year')
+
+print("Renewable electricity consumption (%)")
+print(df3, "\n")
 
 # putting all dataframes in to a list
 data_frames = [df1, df2, df3]
 
-# merging our dataframes into one df
-df_merged = reduce(lambda left, right: pd.merge(left, right, on=['country'],
-                                                how="outer"), data_frames)
+# adding titles and axis names
+df3.plot(x='year', y='value')
+plt.title('China, Renewable Energy Consumption (% of total final energy consumption)')
+plt.ylabel('%')
+plt.xlabel('Date')
 
-# renaming our col names
-df_merged.columns = ["Electricity Access (%)", "Renewable Electricity Excluding Hydro (kWh)",
-                     "Renewable Energy Consumption (%)"]
-
-print(df_merged)
-
-# now we want to visualize some data
-df_merged.plot(x="Electricity Access (%)", y="Renewable Energy Consumption (%)", kind="scatter")
+# plotting renewable energy consumption
 plt.show()
+
